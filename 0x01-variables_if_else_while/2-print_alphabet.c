5b8f9a8d756b554cac43a58da1e52b5e91696781
Abdulbabs <suprconcept@gmail.com>
2022-10-14 09:32:41 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,15 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char low;
-
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 		printf("\n");
-
 	return (0);
-
 }
