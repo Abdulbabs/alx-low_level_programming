42d5c27a4e89e67310611e12ff2ca89a0640ac30
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-15 00:21:31 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,11 @@
  */
 int main(void)
 {
-  char i;
-  
-  for (i = 'z'; i >= 'a'; i--)
-  {
-    putchar(i);
-  }
+	char i;
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
