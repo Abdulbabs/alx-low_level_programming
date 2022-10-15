cd111b921e8c8f4ddf8a65f4996ddf343ec84dd6
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-15 01:34:19 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
- * main-program entry point.
- * Return:0 success, non zero error.
+ *main-program entry point.
+ *Return:0 success, non zero error.
  */
-
 int main(void)
 {
 	int num;
+
 	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(num); 
+		putchar(num);
 		if (num == '9')
 		{
 			break;
