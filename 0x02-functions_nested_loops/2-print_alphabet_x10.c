85d6ff8edcf86314307c6735a5be75d847a0ebd9
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-18 21:48:59 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int n:
+	int n;
 
 	for (n = 0; n <= 9; n++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-    {
+		{
 			_putchar(c);
-    }
+		}
 		_putchar('\n');
 	}
 }
