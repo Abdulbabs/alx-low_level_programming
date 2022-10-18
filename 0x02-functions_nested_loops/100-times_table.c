60e229a3006d5121db20bd3ea0108630c69420e6
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-18 23:04:25 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,40 +8,23 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= n; i++)
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(',');
+			_putchar(' ');
+			k = i * j;
+			if (k <= 9)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				_putchar(' ');
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar((k / 10) + '0')
+				_putchar((k % 10) + '0')
+			}
 		}
-	}
+			_putchar('\n')
 }
