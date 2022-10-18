ccf74bf36524bd3440438c0ae38e3c9c5aedbd82
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-18 21:46:00 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n:
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+    {
+			_putchar(c);
+    }
+		_putchar('\n');
+	}
+}
