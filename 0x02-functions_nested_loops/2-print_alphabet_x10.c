417341827d6e84f1f5b08390f11622137be9de8b
Abdulbabs <suprconcept@gmail.com>
2022-10-18 13:40:39 -0700

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,18 @@
  *
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
 	char c;
-	int n = 0;
+	int n;
 
-	for (; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
+		{
 			_putchar(c);
+		}
 		_putchar('\n');
 	}
 }
