e3eaf9c3f79fe21870984b423fa469a9fa609f08
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-20 09:46:41 -0700

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,13 +18,13 @@ void print_number(int n)
 
 	if (n < 10 && n >= 0)
 	{
-		_putchar (n + '0');
+		putchar (n + '0');
 	}
 	else if (n > -10 && n < 0)
 	{
 		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
+		putchar('-');
+		putchar (n + '0');
 	}
 
 	else
@@ -32,7 +32,7 @@ void print_number(int n)
 		if (n < 0)
 		{
 			n = n * -1;
-			_putchar ('-');
+			putchar ('-');
 		}
 		z = n;
 	while (z / b > 9)
@@ -43,7 +43,7 @@ void print_number(int n)
 	{
 		m = z / b;
 		z = z % b;
-		_putchar (m + '0');
+		putchar (m + '0');
 		b = b / 10;
 	}
 	}
