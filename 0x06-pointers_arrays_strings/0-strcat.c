17829de62ba86069819f79daf005eec50fa23ffe
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-27 11:40:26 +0100

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
-/*
- *
- *_strcat - a function that concatenates two strings
- *
- * @dest: string
- *
- * @src: string
- *
- * Return: string
- *
- */
+/**
+* *_strcat - a function that concatenates two strings
+* @dest: string
+* @src: string
+* Return: string
+**/
 
 char *_strcat(char *dest, char *src)
 {
 	int l;
 	int i;
+
 	for (l = 0; dest[l] != '\0'; l++)
 	{
 	}
