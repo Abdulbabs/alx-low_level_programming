59f0eca7bf1ae76e646c523e33cd545120ce41ec
Abdulkareem Muhammed Babatunde <113603890+Abdulbabs@users.noreply.github.com>
2022-10-31 14:14:20 +0100

    Set String

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,14 +1,9 @@
 #include "main.h"
-
 /**
- *
- * * set_string - function that sets the value of a pointer to a char
- *
- * *@s: pointer to string
- *
- * *@to: pointer to string
- *
- * **/
+* set_string - function that sets the value of a pointer to a char
+*@s: pointer to string
+*@to: pointer to string
+**/
 
 void set_string(char **s, char *to)
 {
